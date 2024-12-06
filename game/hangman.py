bb6aa02167a4eef558e1043cb32bb3802276fb06
Thabile Mabaso <tmabaso123@student.wethinkcode.co.za>
2024-12-06 17:34:53 +0200

    added a function to guess the letter from the word

--- game/hangman.py
+++ game/hangman.py
@@ -4,3 +4,6 @@ words = ["beach", "squid", "laugh", "grapes", "ground"]
 
 def select_random_word_from_words(words):
     return random.choice(words)
+
+def guess_the_letter():
+    return input("make a guess: ")
