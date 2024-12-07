55edc49fc81f5ed10262cefcf8619c48730584c5
Thabile Mabaso <tmabaso123@student.wethinkcode.co.za>
2024-12-07 08:48:47 +0200

    added a function to get the index of the guessed word

--- game/hangman.py
+++ game/hangman.py
@@ -7,3 +7,13 @@ def select_random_word_from_words(words):
 
 def guess_the_letter():
     return input("make a guess: ")
+
+def get_index_of_guess(guess, word):
+    index = 0
+    for item in word:
+        if guess != item:
+            index += 1
+        else:
+            break
+
+    return index
